2fcc4a299e0fd992c794a70169a5b9918112bf19
Julián <julianlopezcastillo12@gmail.com>
2024-11-27 00:48:41 -0400

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Form } from "react-hook-form";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/FormAsig.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from "react";
+import { useForm } from "react-hook-form";
+
+const form = () => {
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <>
+      <h2>Editar asignatura</h2>
+
+      <form>
+        <div className="form-control">
+          <label>Nombre</label>
+          <input type="text" {...register("name")} />
+        </div>
+        <div className="form-control">
+          <label>Unidades de crédito</label>
+          <input type="number" {...register("credit units")} />
+        </div>
+        <div className="form-control">
+          <label>Profesor</label>
+          <input type="text" {...register("teacher")} />
+        </div>
+        <div className="form-control">
+          <label>Descripción</label>
+          <input type="text" {...register("description")} />
+        </div>
+        <div className="form-control">
+          <label>Horario</label>
+          <input type="text" {...register("schedule")} />
+        </div>
+        <div className="form-control">
+          <label>Aula</label>
+          <input type="number" {...register("classroom")} />
+        </div>
+        <div className="form-control">
+          <label>Prerrequisitos</label>
+          <input type="text" {...register("prerequisites")} />
+        </div>
+        <div className="form-control">
+          <label>Cupo máximo</label>
+          <input type="number" {...register("maximum quota")} />
+        </div>
+        <button type="submit">Actualizar datos</button>
+      </form>
+    </>
+  );
+};
+
+export default form;
