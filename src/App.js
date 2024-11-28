fd1e77dde7ed9ade30719178311a76f8241169f8
Julián <julianlopezcastillo12@gmail.com>
2024-11-28 16:50:04 -0400

    s

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import "./components/Menú.css"
 import FormValidation from "./components/FormValidation";
-import MenúNavegación from "./components/MenúNavegación";
 
 function App() {
   return (
     <div className="container">
       <FormValidation />
-      <MenúNavegación/>
     </div>
   )
 }
