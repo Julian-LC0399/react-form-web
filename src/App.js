b6eb634457852b362341d0107941c7f2c27f5079
Julián <julianlopezcastillo12@gmail.com>
2024-11-28 18:00:56 -0400

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 import Navbar from './components/Navbar';
 import FormValidation from "./components/FormValidation";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reports from "./pages/Reports";
-import Products from "./pages/Products";
+import { BrowserRouter as Router} from 'react-router-dom';
+
 
 function App() {
   return (
@@ -12,11 +10,6 @@ function App() {
       <Router>
       <FormValidation />
         <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
-        </Switch>
       </Router>
     </div>
   )
