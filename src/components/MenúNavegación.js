75b6d6a48800b3c84ab7aba35c3ea81666b93444
Julián <julianlopezcastillo12@gmail.com>
2024-11-28 16:18:38 -0400

    t

--- src/components/MenúNavegación.js
+++ src/components/MenúNavegación.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { IconContext } from 'react-icons';
+import { Link } from 'react-router-dom';
+import {SiderbarData} from "./SiderbarData";
+
 
 function MenúNavegación() {
-  return (
-    <div>MenúNavegación</div>
-  )
-}
-
-export default MenúNavegación
+    const [sidebar, setSidebar] = useState(false);
+  
+    const showmenú = () => setSidebar(!sidebar);
+  
+    return (
+      <>
+        <IconContext.Provider value={{ color: '#fff' }}>
+          <div className='navbar'>
+            <Link to='#' className='menu-bars'>
+              <FaIcons.FaBars onClick={showmenú} />
+            </Link>
+          </div>
+          <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className='nav-menu-items' onClick={showmenú}>
+              <li className='navbar-toggle'>
+                <Link to='#' className='menu-bars'>
+                  <AiIcons.AiOutlineClose />
+                </Link>
+              </li>
+              {SiderbarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </IconContext.Provider>
+      </>
+    );
+  }
+  
+  export default MenúNavegación;
