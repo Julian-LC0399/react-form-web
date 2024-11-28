585c761206c70ef05a52e6bcdaec44a03add7c1f
Julián <julianlopezcastillo12@gmail.com>
2024-11-28 14:56:56 -0400

    mejoras

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import "./components/Menú.css"
 import FormValidation from "./components/FormValidation";
+import MenúNavegación from "./components/MenúNavegación";
 
 function App() {
   return (
     <div className="container">
       <FormValidation />
+      <MenúNavegación/>
     </div>
   )
 }
---
+++ src/components/SiderbarData.js
@@ -0,0 +1,31 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Dashoard",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Estudiantes",
+    path: "/estudiantes",
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+  },
+  {
+    title: "Profesores",
+    path: "/profesores",
+    icon: <FaIcons.FaCartPlus />,
+    cName: "nav-text",
+  },
+  {
+    title: "Asignaturas",
+    path: "/asignaturas",
+    icon: <IoIcons.IoMdPeople />,
+    cName: "nav-text",
+  },
+];
